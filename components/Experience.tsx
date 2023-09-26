cff438da5e5a4905e81de6c7d1e1381003d2ce82
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-09-26 14:10:51 +0500

    Update experiences

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -34,32 +34,26 @@ const Experience = () => {
          <SectionTitle title='where I have worked'/>
          <div className='w-full mt-10 flex flex-col md:flex-row gap-16'>
             <ul className='md:w-32 flex flex-col'>
+                                
+            <li onClick={handleMicroverse}
+                className={`${workMicroverse ? 'border-l-textGreen text-textGreen':'border-l-hoverColor text-textDark'}
+                border-l-2 bg:transparent hover:bg-[#112240] py-3 text-sm
+                cursor-pointer duration-300 px-8 font-medium`}>
+                    Microverse
+                </li>
+
                 <li onClick={handleMustfintect}
                 className={`${workMustfintect ? 'border-l-textGreen text-textGreen':'border-l-hoverColor text-textDark'}
                 border-l-2 bg:transparent hover:bg-[#112240] py-3 text-sm
                 cursor-pointer duration-300 px-8 font-medium`}>
                     Must Fintect
                 </li>
-                
-                <li onClick={handleMicroverse}
-                className={`${workMicroverse ? 'border-l-textGreen text-textGreen':'border-l-hoverColor text-textDark'}
-                border-l-2 bg:transparent hover:bg-[#112240] py-3 text-sm
-                cursor-pointer duration-300 px-8 font-medium`}>
-                    Microverse
-                </li>
-{/* 
-                <li onClick={handleMustfintect}
-                className={`${workMustfintect ? 'border-l-textGreen text-textGreen':'border-l-hoverColor text-textDark'}
-                border-l-2 bg:transparent hover:bg-[#112240] py-3 text-sm
-                cursor-pointer duration-300 px-8 font-medium`}>
-                    Must fintect
-                </li> */}
 
                 <li onClick={handleBajCo}
                 className={`${workBajco ? 'border-l-textGreen text-textGreen':'border-l-hoverColor text-textDark'}
                 border-l-2 bg:transparent hover:bg-[#112240] py-3 text-sm
                 cursor-pointer duration-300 px-8 font-medium`}>
-                    BajCo
+                    SIGNITIVE LOGICS
                 </li>
 
             </ul>
--- components/works/BajCo.tsx
+++ components/works/BajCo.tsx
@@ -11,32 +11,32 @@ const BajCo = () => {
           className="w-full"
         >
           <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-          Mentor (Volunteer)
-            <span className="text-textGreen tracking-wide">@Microverse</span>
+          Front-end Developer 
+            <span className="text-textGreen tracking-wide">@SIGNITIVE LOGICS</span>
           </h3>
           <p className="text-sm mt-1 font-medium text-textDark">
-            Jan 2023 - Present{" "}
+            Feb 2021 - Feb 2023{" "}
           </p>
           <ul>
             <li className="text-base flex gap-2 text-textDark">
               <span className="text-textGreen mt-1">
                 <TiArrowForward />
               </span>{" "}
-              Mentoring junior web developers, and providing technical support through code reviews.
+              Demonstrated expertise in responsive web design, utilizing HTML, CSS, and JavaScript to achieve a 25% increase in user engagement.
             </li>
     
             <li className="text-base flex gap-2 text-textDark">
               <span className="text-textGreen mt-1">
                 <TiArrowForward />
               </span>{" "}
-              Proposing improvements to code organization to improve code quality and overall performance.
+              Translated wireframes into ﬂawless interfaces, resulting in a notable 20% boost in conversion rates.
             </li>
     
             <li className="text-base flex gap-2 text-textDark">
               <span className="text-textGreen mt-1">
                 <TiArrowForward />
               </span>{" "}
-              Providing advice and tips on maintaining motivation to maintain longevity in the program.
+              Collaborated effectively to streamline integration, leading to a 30% reduction in bug reports and improved user experiences.
             </li>
           </ul>
         </motion.div>
