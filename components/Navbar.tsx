07072bce9ae8b6e8dc7e118584888a82afd934ac
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-09-26 14:00:34 +0500

    Fix overlay bug

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,95 +1,271 @@
-/* eslint-disable @next/next/no-img-element */
-import Link from 'next/link'
-import logoImage from '../public/logoImage.png'
-
-import { motion } from 'framer-motion'
+import Link from "next/link";
+import logoImage from "../public/logoImage.png";
+import { useRef, useState } from "react";
+import { delay, motion } from "framer-motion";
+import { MdOutlineClose } from "react-icons/md";
 
 const Navbar = () => {
+  const ref = useRef<string | any>("");
+  const [showMenu, setShowMenu] = useState(false);
+
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
+  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    const href = e.currentTarget.href;
+    const targetId = href.replace(/.*\#/, "");
+    const elem = document.getElementById(targetId);
+    elem?.scrollIntoView({
+      behavior: "smooth",
+    });
+    const links = document.querySelectorAll(".nav-link");
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+    closeMenu(); // Close the menu when a menu item is selected
+  };
+
   return (
-    <div className='w.full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4'>
-      <div className='max-w-container h-full mx-auto py-1 font-titleFont flex intem-center justify-between'>
+    <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
+      <div className="max-w-container h-full mx-auto py-1 font-titleFont flex intem-center justify-between">
         <motion.div
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        transition={{duration: 0.5}}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
         >
-          <img src={logoImage.src} alt="logo" 
-          className='w-20 h-auto'/>
+          <img src={logoImage.src} alt="logo" className="w-20 h-auto" />
         </motion.div>
-        <div className='hidden mdl:inline-flex items-center gap-7'>
-          <ul className='flex text-[14px] gap-7'>
-          {/* <ul className='flex text-[13px] gap-7'> */}
-            <Link href='#home' className='flex items-center gap-1 font-medium text-textDrak
-            hover:text-textGreen cursor-pointer duration-300 nav-link'>
-            <motion.li
-            initial={{ y:-10 , opacity: 0}}
-            animate={{ y:0 , opacity: 1}}
-            transition={{duration: 0.2}}
-            >Home</motion.li>
+        <div className="hidden mdl:inline-flex items-center gap-7">
+          <ul className="flex text-[14px] gap-7">
+            <Link
+              href="#home"
+              onClick={handleScroll}
+              className="flex items-center gap-1 font-medium text-textDrak
+            hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.2 }}
+              >
+                Home
+              </motion.li>
             </Link>
-            <Link href='#about' className='flex items-center gap-1 font-medium text-textDrak
-            hover:text-textGreen cursor-pointer duration-300 nav-link'>
-            <motion.li
-            initial={{ y:-10 , opacity: 0}}
-            animate={{ y:0 , opacity: 1}}
-            transition={{duration: 0.2}}>
-            About
-            </motion.li>
+            <Link
+              href="#about"
+              onClick={handleScroll}
+              className="flex items-cenår gap-1 font-medium text-textDrak
+            hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.2 }}
+              >
+                About
+              </motion.li>
             </Link>
-            <Link href='#experience' className='flex items-center gap-1 font-medium text-textDrak
-            hover:text-textGreen cursor-pointer duration-300 nav-link'>
-            <motion.li
-            initial={{ y:-10 , opacity: 0}}
-            animate={{ y:0 , opacity: 1}}
-            transition={{duration: 0.2}}>
-            Experience
-            </motion.li>
+            <Link
+              href="#experience"
+              onClick={handleScroll}
+              className="flex items-center gap-1 font-medium text-textDrak
+            hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.2 }}
+              >
+                Experience
+              </motion.li>
             </Link>
-            <Link href='#project' className='flex items-center gap-1 font-medium text-textDrak
-            hover:text-textGreen cursor-pointer duration-300 nav-link'>
-            <motion.li
-            initial={{ y:-10 , opacity: 0}}
-            animate={{ y:0 , opacity: 1}}
-            transition={{duration: 0.2}}>
-            Projects
-            </motion.li>
+            <Link
+              href="#project"
+              onClick={handleScroll}
+              className="flex items-center gap-1 font-medium text-textDrak
+            hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.2 }}
+              >
+                Projects
+              </motion.li>
             </Link>
-            <Link href='#contact' className='flex items-center gap-1 font-medium text-textDrak
-            hover:text-textGreen cursor-pointer duration-300 nav-link'>
-            <motion.li
-            initial={{ y:-10 , opacity: 0}}
-            animate={{ y:0 , opacity: 1}}
-            transition={{duration: 0.2}}>
-            Contact
-            </motion.li>
+            <Link
+              href="#contact"
+              onClick={handleScroll}
+              className="flex items-center gap-1 font-medium text-textDrak
+            hover:text-textGreen cursor-pointer duration-300 nav-link"
+            >
+              <motion.li
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.2 }}
+              >
+                Contact
+              </motion.li>
             </Link>
           </ul>
-          <a href='assets/Resume.pdf' target='_blank'>
-          <motion.button
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        transition={{duration: 0.5}}
-        className='px-4 py-2 rounded-md text-textGreen text-[14px] border border-textGreen
-        hover:bg-hoverColor duration-300'>
-          Resume
-        </motion.button>
-        </a>
+          <a href="assets/Resume.pdf" target="_blank">
+            <motion.button
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              className="px-4 py-2 rounded-md text-textGreen text-[14px] border border-textGreen
+        hover:bg-hoverColor duration-300"
+            >
+              Resume
+            </motion.button>
+          </a>
         </div>
-        <div className='w-8 h-5 mt-5 flex flex-col justify-between items-center mdl:hidden text-hxl
-        text-textGreen cursor-pointer overflow-hidden group'>
-          <span className='w-full h-[2px] bg-textGreen inline-flex transform 
-          group-hover:translate-x-2 transition-all ease-in-out duration-300'>
-          </span>
-          <span className='w-full h-[2px] bg-textGreen inline-flex transform 
-          group-hover:translate-x-2 transition-all ease-in-out duration-300'>
-          </span>
-          <span className='w-full h-[2px] bg-textGreen inline-flex transform 
-          group-hover:translate-x-2 transition-all ease-in-out duration-300'>
-          </span>
+        {/* small button */}
+        <div
+          onClick={() => {
+            setShowMenu(true);
+          }}
+          className="w-8 h-5 mt-5 flex flex-col justify-between items-center mdl:hidden text-hxl
+        text-textGreen cursor-pointer overflow-hidden group"
+        >
+          <span
+            className="w-full h-[2px] bg-textGreen inline-flex transform 
+          group-hover:translate-x-2 transition-all ease-in-out duration-300"
+          ></span>
+          <span
+            className="w-full h-[2px] bg-textGreen inline-flex transform 
+          group-hover:translate-x-2 transition-all ease-in-out duration-300"
+          ></span>
+          <span
+            className="w-full h-[2px] bg-textGreen inline-flex transform 
+          group-hover:translate-x-2 transition-all ease-in-out duration-300"
+          ></span>
         </div>
+        {showMenu && (
+          <div
+            ref={(node) => {
+              ref.current = node;
+            }}
+            className="absolute mdl:hidden top-0 right-0 w-full h-screen bg-black
+            bg-opacity-50 flex flex-col items-end"
+          >
+            <motion.div
+              initial={{ x: 20, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.1 }}
+              className="w-full min-h-full overflow-y-scroll scrollbarHide bg-[#112240]
+              flex flex-col items-center justify-center px-4 py-10 relative"
+            >
+              <MdOutlineClose
+                onClick={() => {
+                  setShowMenu(false);
+                }}
+                className="text-3xl text-textGreen cursor-pointer hover:text-red-500 absolute top-4 right-4"
+              />
+
+              <div className="flex flex-col items-center gap-7">
+                <ul className="flex flex-col text-base gap-7">
+                  <Link
+                    href="#home"
+                    onClick={handleScroll}
+                    className="flex items-center gap-1 font-medium text-textDrak
+            hover:text-textGreen cursor-pointer duration-300 nav-link"
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        duration: 0.2,
+                        delay: 0.1,
+                        ease: "easeIn",
+                      }}
+                    >
+                      Home
+                    </motion.li>
+                  </Link>
+                  <Link
+                    href="#about"
+                    onClick={handleScroll}
+                    className="flex items-center gap-1 font-medium text-textDrak
+            hover:text-textGreen cursor-pointer duration-300 nav-link"
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        duration: 0.2,
+                        delay: 0.2,
+                        ease: "easeIn",
+                      }}
+                    >
+                      About
+                    </motion.li>
+                  </Link>
+                  <Link
+                    href="#experience"
+                    onClick={handleScroll}
+                    className="flex items-center gap-1 font-medium text-textDrak
+            hover:text-textGreen cursor-pointer duration-300 nav-link"
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        duration: 0.2,
+                        delay: 0.3,
+                        ease: "easeIn",
+                      }}
+                    >
+                      Experience
+                    </motion.li>
+                  </Link>
+                  <Link
+                    href="#project"
+                    onClick={handleScroll}
+                    className="flex items-center gap-1 font-medium text-textDrak
+            hover:text-textGreen cursor-pointer duration-300 nav-link"
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        duration: 0.2,
+                        delay: 0.4,
+                        ease: "easeIn",
+                      }}
+                    >
+                      Projects
+                    </motion.li>
+                  </Link>
+                  <Link
+                    href="#contact"
+                    onClick={handleScroll}
+                    className="flex items-center gap-1 font-medium text-textDrak
+            hover:text-textGreen cursor-pointer duration-300 nav-link"
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{
+                        duration: 0.2,
+                        delay: 0.5,
+                        ease: "easeIn",
+                      }}
+                    >
+                      Contact
+                    </motion.li>
+                  </Link>
+                </ul>
+              </div>
+            </motion.div>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
