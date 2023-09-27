93a22328665981418e3d8d712e6192ab4d7b3983
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-09-27 14:14:22 +0500

    Add form to the portfolio website

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,32 +1,84 @@
-import React from 'react'
-import SectionTitle from './SectionTitle'
+import React from 'react';
+import SectionTitle from './SectionTitle';
 
 const Contact = () => {
   return (
-    <section 
-    id='contact'
-    className='max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4
-    items-center justify-center'
+    <section
+      id='contact'
+      className='max-w-contentContainer mx-auto py-10 xl:py-32 flex flex-col gap-4 items-center justify-center'
     >
-        <p className='font-titleFont text-lg text-textGreen font-semibold flex items-center tracking-wide'>
-            What&apos;s Next?
-        </p>
+      <p className='font-titleFont text-lg text-textGreen font-semibold flex items-center tracking-wide'>
+        What&apos;s Next?
+      </p>
 
-        <h2 className='font-titleFont text-5xl font-semibold' >Get In Touch</h2>
+      <h2 className='font-titleFont text-5xl font-semibold'>Get In Touch</h2>
 
-        <p className='max-w-[600px] text-center text-textDark'>
+      <p className='max-w-[600px] text-center text-textDark'>
         I’m currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!
-        </p>
+      </p>
 
-        <a href="mailto:atifkhatak242@gamil.com">
-            {" "}
-        <button className='w-40 h-14 border border-textGreen mt-6 font-titleFont text-sm
-        text-textGreen tracking-wide rounded-md hover:bg-hoverColor duration-300'>
-            Say Hello</button>
-        </a>
-
+      {/* Simple Form */}
+      <form
+        action='mailto:atifkhatak242@gmail.com'
+        method='post'
+        enctype='text/plain'
+        className='mt-6 w-[50%] '
+      >
+        <div className='mb-4'>
+          <label
+            htmlFor='name'
+            className='block text-textDark font-titleFont text-sm mb-2'
+          >
+            Name
+          </label>
+          <input
+            type='text'
+            id='name'
+            name='name'
+            className='w-full px-4 py-2 border border-textGreen rounded-md focus:outline-none focus:border-hoverColor'
+            required
+          />
+        </div>
+        <div className='mb-4'>
+          <label
+            htmlFor='email'
+            className='block text-textDark font-titleFont text-sm mb-2'
+          >
+            Email
+          </label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            className='w-full px-4 py-2 border border-textGreen rounded-md focus:outline-none focus:border-hoverColor'
+            required
+          />
+        </div>
+        <div className='mb-4'>
+          <label
+            htmlFor='message'
+            className='block text-textDark font-titleFont text-sm mb-2'
+          >
+            Message
+          </label>
+          <textarea
+            id='message'
+            name='message'
+            rows='4'
+            className='w-full px-4 py-2 border border-textGreen rounded-md focus:outline-none focus:border-hoverColor'
+            placeholder='Write your message'
+            required
+          />
+        </div>
+        <button
+          type='submit'
+          className='w-40 h-14 border border-textGreen font-titleFont text-sm text-textGreen tracking-wide rounded-md hover:bg-hoverColor duration-300'
+        >
+          Say Hello
+        </button>
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
